23d7fc56caddabf8c825cafb3d750891b8093c0c
xing-6464 <xing_6464@foxmail.com>
2023-03-24 17:56:11 +0800

    整型

--- chapter1/data_type.c
+++ chapter1/data_type.c
@@ -5,14 +5,27 @@ int main(int argc, char const *argv[])
 {
     short int short_int = 0;
     int i = 0;
-    long int long_int = 0;
+    long long_int = 0;
     long long longlong_int = 0;
 
+    unsigned int unsigned_int = 123;
+    unsigned long unsigned_long = 234;
+
+    
+    size_t sizeof_int = sizeof(int);
+    
+    // d = decimal
+    // x = hex
+    // o = oct
+    // %hd short decimal
+    // %hu unsinged short int 
     // %d decimal
-    printf("short int: %d\n", sizeof(unsigned short int));
-    printf("int: %d\n", sizeof(unsigned int));
-    printf("long int: %d\n", sizeof(unsigned long int));
-    printf("long long int: %d\n", sizeof(unsigned long long int));
+    // %ld long decimal
+    // %lld long long decimal
+    printf("short int: %lu\n", sizeof(unsigned short int));
+    printf("int: %lu\n", sizeof(unsigned int));
+    printf("long int: %lu\n", sizeof(unsigned long int));
+    printf("long long int: %lu\n", sizeof(unsigned long long int));
 
     printf("max int %d, min %d\n", INT_MAX, INT_MIN);
     printf("usingned max int %u, usingned min %u\n", UINT_MAX, 0);
